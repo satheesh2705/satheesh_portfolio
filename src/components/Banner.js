6952529c31a2db29dbcfd8ae938c9e6b91be2f9a
maithenkumar <maitheen1905@gmail.com>
2023-09-14 15:10:56 +0530

    resume updated

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,17 +1,21 @@
 import { useState, useEffect } from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import headerImg from "../assets/img/header-img.png";
-import { ArrowRightCircle } from 'react-bootstrap-icons';
-import 'animate.css';
-import TrackVisibility from 'react-on-screen';
+import { ArrowRightCircle } from "react-bootstrap-icons";
+import "animate.css";
+import TrackVisibility from "react-on-screen";
 
 export const Banner = () => {
   const [loopNum, setLoopNum] = useState(0);
   const [isDeleting, setIsDeleting] = useState(false);
-  const [text, setText] = useState('');
+  const [text, setText] = useState("");
   const [delta, setDelta] = useState(300 - Math.random() * 100);
   const [index, setIndex] = useState(1);
-  const toRotate = [ "FullStack Develop", "FullStack Develop", "FullStack Develop" ];
+  const toRotate = [
+    "FullStack Develop",
+    "FullStack Develop",
+    "FullStack Develop",
+  ];
   const period = 2000;
 
   useEffect(() => {
@@ -19,33 +23,37 @@ export const Banner = () => {
       tick();
     }, delta);
 
-    return () => { clearInterval(ticker) };
-  }, [text])
+    return () => {
+      clearInterval(ticker);
+    };
+  }, [text]);
 
   const tick = () => {
     let i = loopNum % toRotate.length;
     let fullText = toRotate[i];
-    let updatedText = isDeleting ? fullText.substring(0, text.length - 1) : fullText.substring(0, text.length + 1);
+    let updatedText = isDeleting
+      ? fullText.substring(0, text.length - 1)
+      : fullText.substring(0, text.length + 1);
 
     setText(updatedText);
 
     if (isDeleting) {
-      setDelta(prevDelta => prevDelta / 2);
+      setDelta((prevDelta) => prevDelta / 2);
     }
 
     if (!isDeleting && updatedText === fullText) {
       setIsDeleting(true);
-      setIndex(prevIndex => prevIndex - 1);
+      setIndex((prevIndex) => prevIndex - 1);
       setDelta(period);
-    } else if (isDeleting && updatedText === '') {
+    } else if (isDeleting && updatedText === "") {
       setIsDeleting(false);
       setLoopNum(loopNum + 1);
       setIndex(1);
       setDelta(500);
     } else {
-      setIndex(prevIndex => prevIndex + 1);
+      setIndex((prevIndex) => prevIndex + 1);
     }
-  }
+  };
 
   return (
     <section className="banner" id="home">
@@ -53,33 +61,72 @@ export const Banner = () => {
         <Row className="aligh-items-center">
           <Col xs={12} md={6} xl={7}>
             <TrackVisibility>
-              {({ isVisible }) =>
-              <div className={isVisible ? "animate__animated animate__fadeIn" : ""}>
-                <span className="tagline">Welcome to my Portfolio</span>
-                <h1><h3>Hi! I'am </h3> Maithenkumar <span className="txt-rotate" dataPeriod="1000" data-rotate='[ "Web Developer", "Web Designer", "UI/UX Designer" ]'><span className="wrap">{text}</span></span></h1>
-                  <p>Helo! My name Maithenkumar, and I am a passionate Full Stack Developer. With one year of professional experience and a strong foundation in web development, I am constantly honing my skills and expanding my knowledge.<br/>
-                  <br/>
-
-I love working on exciting projects, both in my current role and through freelancing opportunities. These experiences have given me the chance to collaborate with diverse teams and clients, enriching my problem-solving abilities and creativity.<br/>
-<br/>
-Currently in my final year of study, I am eager to showcase my work and journey as a developer through this portfolio. Here, you will find a collection of my latest projects, experiments, and accomplishments. I am dedicated to creating clean and user-friendly solutions, always aiming to leave a positive impact on the web.</p>
-                  <button onClick={() => console.log('connect')}>Let’s Connect <ArrowRightCircle size={25} /></button>
-              </div>}
+              {({ isVisible }) => (
+                <div
+                  className={
+                    isVisible ? "animate__animated animate__fadeIn" : ""
+                  }
+                >
+                  <span className="tagline">Welcome to my Portfolio</span>
+                  <h1>
+                    <h3>Hi! I'am </h3> Maithenkumar{" "}
+                    <span
+                      className="txt-rotate"
+                      dataPeriod="1000"
+                      data-rotate='[ "Web Developer", "Web Designer", "UI/UX Designer" ]'
+                    >
+                      <span className="wrap">{text}</span>
+                    </span>
+                  </h1>
+                  <p>
+                    Helo! My name Maithenkumar, and I am a passionate Full Stack
+                    Developer. With one year of professional experience and a
+                    strong foundation in web development, I am constantly honing
+                    my skills and expanding my knowledge.
+                    <br />
+                    <br />
+                    I love working on exciting projects, both in my current role
+                    and through freelancing opportunities. These experiences
+                    have given me the chance to collaborate with diverse teams
+                    and clients, enriching my problem-solving abilities and
+                    creativity.
+                    <br />
+                    <br />
+                    Currently in my final year of study, I am eager to showcase
+                    my work and journey as a developer through this portfolio.
+                    Here, you will find a collection of my latest projects,
+                    experiments, and accomplishments. I am dedicated to creating
+                    clean and user-friendly solutions, always aiming to leave a
+                    positive impact on the web.
+                  </p>
+                  <button onClick={() => console.log("connect")}>
+                    Let’s Connect <ArrowRightCircle size={25} />
+                  </button>
+                </div>
+              )}
             </TrackVisibility>
           </Col>
           <Col xs={15} md={8} xl={5}>
-         
-          <div>
-            <TrackVisibility>
-              {({ isVisible }) =>
-                <div className={isVisible ? "animate__animated animate__zoomIn" : ""}>
-                  <img className="maitheen-img" src={headerImg} alt="Header Img"/>
-                </div>}
-            </TrackVisibility>
+            <div>
+              <TrackVisibility>
+                {({ isVisible }) => (
+                  <div
+                    className={
+                      isVisible ? "animate__animated animate__zoomIn" : ""
+                    }
+                  >
+                    <img
+                      className="maitheen-img"
+                      src={headerImg}
+                      alt="Header Img"
+                    />
+                  </div>
+                )}
+              </TrackVisibility>
             </div>
           </Col>
         </Row>
       </Container>
     </section>
-  )
-}
+  );
+};
