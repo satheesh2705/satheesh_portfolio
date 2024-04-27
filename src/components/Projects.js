ecfc3ad798025437b8ff7b8f3a655ebdb62a9108
satheesh2705 <mrsatheesh2705@gmail.com>
2024-04-27 18:07:28 +0530

    changes_apr28

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,10 +1,11 @@
 import { Container, Row, Col, Tab, Nav } from "react-bootstrap";
 import { ProjectCard } from "./ProjectCard";
-import projImg1 from "../assets/prj/wheather-app.png";
-import projImg2 from "../assets/prj/blog-ui.jpeg";
-import projImg3 from "../assets/prj/chat-gpt.png";
-import projImg4 from "../assets/prj/cycle_ui.jpeg";
+import projImg1 from "../assets/prj/social_media_app_reactjs.png";
+import projImg2 from "../assets/prj/blog_app.png";
+import projImg3 from "../assets/prj/weather_app.png";
+import projImg4 from "../assets/prj/E-commerce.png";
 import projImg5 from "../assets/prj/insta-ui.jpeg";
+import projImg6 from "../assets/prj/zomato_clone.jpeg";
 import colorSharp2 from "../assets/img/keyboard.jpg";
 import 'animate.css';
 import TrackVisibility from 'react-on-screen';
@@ -13,35 +14,40 @@ export const Projects = () => {
 
   const projects = [
     {
-      title: "Wheather_application",
-      description: "API Integration for App",
-      imgUrl: projImg3,
+      title: "SocialMedia Real Time chats and Posts",
+      description: "Dynamic & Responsive App Using React.js ",
       imgUrl: projImg1,
-      link:''
+      link:'https://github.com/satheesh2705/React_social_media'
     },
     {
-      title: "Blog_application",
-      description: "UI for Mobile App using Flutter",
+      title: "Blog_Application",
+      description: "Mobile App using Flutter UI",
       imgUrl: projImg2,
-      link:''
+      link:'#'
     },
     {
-      title: "Chat_GPT",
-      description: "API Integration for Mobile App",
+      title: "Weather_Application",
+      description: "A user-friendly Flutter weather application utilizing a free REST API for accurate weather forecasts.",
       imgUrl: projImg3,
-      link:''
+      link:'https://github.com/satheesh2705/wheather_app_flutter'
     },
     {
-      title: "Cycle_store",
-      description: "E-commerce for web using Html&Css",
+      title: "E-Commerce website",
+      description: "Dynamic & Responsive Website By Using Html,Css & JavaScript" ,
       imgUrl: projImg4,
-      link:''
+      link:'https://github.com/satheesh2705/E-commerce-website'
+    },
+    {
+      title: "ZOMATO_UI",
+      description: "Zomato clone built with HTML and CSS, replicating the interface",
+      imgUrl: projImg6, 
+      link:'https://github.com/satheesh2705/Zomata_Web'
     },
     {
       title: "Instagram_UI",
-      description: "Html &Css for web",
+      description: "Instagram clone built with HTML and CSS, replicating the interface and functionality",
       imgUrl: projImg5,
-      link:''
+      link:'https://github.com/satheesh2705/instagram_ui'
     }
    
   ];
